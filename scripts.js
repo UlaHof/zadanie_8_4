c8d3bd4acdbdd3a53b304f26aebd9d8d879f58e5
Ula <chcetylkoobejrzecfilm@gmail.com>
2017-11-28 22:57:54 +0100

    Add scripts.js

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// scripts.js
+
+/*
+The prompt function below displays field for name input
+*/
+var name = prompt('Enter your name');
+// alert('Hello, ' + name);
+
+// The function below displays Hello and inputed name
+console.log('Hello, ' + name);
